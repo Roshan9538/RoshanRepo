138202f0fc8a447ec8c9a885a390c55064936d7b
rosha <rosha@192.168.2.30>
2018-07-21 14:18:08 +0530

    Roshan1st commit

---
+++ GithubTest/src/Hello.java
@@ -0,0 +1,18 @@
+
+public class Hello {
+	
+	public void disp()
+	{
+		System.out.println("Hello Roshan");
+	}
+	
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hello h = new Hello();
+		h.disp();
+
+	}
+
+}
